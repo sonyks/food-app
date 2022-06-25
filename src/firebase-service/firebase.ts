bfec4d7bd89b6d5caab3e8bd6b900deef85d213b
Oleksandr Poliukhovych <sonyks2007@ukr.net>
2022-06-25 10:00:40 +0300

    fixed issue with exposed keys

--- src/firebase-service/firebase.ts
+++ src/firebase-service/firebase.ts
@@ -11,15 +11,15 @@ import { CheckoutOrder } from "../components/Cart/models/checkout-order.model";
 import { Meal } from "../models/meal.model";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOD6DhDNjwtkjOsMJ9E5ehLcaARfNWpaY",
-  authDomain: "food-app-bc88f.firebaseapp.com",
-  databaseURL:
-    "https://food-app-bc88f-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "food-app-bc88f",
-  storageBucket: "food-app-bc88f.appspot.com",
-  messagingSenderId: "327111936113",
-  appId: "1:327111936113:web:a2ead1414157274b4df3d0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
 };
+
 const app = initializeApp(firebaseConfig);
 
 export const getMeals = async (): Promise<Meal[]> => {
